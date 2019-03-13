8fd1b3655b0ee8f4cd565efcb8dd0ef4e57e9c06
hamza <fk_zaidi@esi.dz>
2019-03-13 09:55:39 +0100

    Added Some permission classes to accounts module

--- src/accounts/permissions.py
+++ src/accounts/permissions.py
@@ -8,3 +8,17 @@ class IsAutomobiliste(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return request.user.is_automobiliste
+
+class IsAdminFabriquant(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user.is_admin_fabriquant
+
+class IsUserFabriquant(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user.is_fabriquant
+
+class IsAdminstrateur(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_admin
